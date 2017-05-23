89e25032a1d97af05d2559d5eb4335fb0f315266
adarshjain9628@gmail.com <aduilu2k15>
2017-05-23 23:35:32 +0530

    Started multiplayer

---
+++ Uncompressed/custom-js/myjs.js
@@ -0,0 +1,238 @@
+$(function() {
+    // $(".playstore").removeClass("right").addClass("center-page").removeClass('visi');
+    // updateContainer();
+    // $(window).resize(function() {
+    //     updateContainer();
+    // });
+    $(".above").on('click', function() {
+        Materialize.toast('You cannot play there!', 1500);
+    });
+    $('.modal').modal();
+    playGame();
+});
+var player = 0;
+var matrix = [
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+    2, 2, 2, 2, 2, 2, 2, 2, 2,
+];
+var big = [2, 2, 2,
+    2, 2, 2,
+    2, 2, 2
+];
+var minicount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var megacount = 0;
+
+function playGame() {
+    $(".box-small").on('click', function() {
+        if(!$(this).hasClass('done')) {
+            matrix[$(this).attr('id')] = player;
+            var currPos = $(this).attr('id');
+            var cBigPos = getBig(currPos);
+            minicount[cBigPos] += 1;
+            if(player == 0) {
+                $(this).append('<img src="asset/x.jpg" class="xo-img"></img>');
+                checkSmall(currPos, cBigPos);
+                player = 1;
+            } else if(player == 1) {
+                $(this).append('<img src="asset/o.jpg" class="xo-img"></img>');
+                checkSmall(currPos, cBigPos);
+                player = 0;
+            }
+            block(nextBigPos(currPos));
+            $(this).addClass('done');
+        } else {
+            Materialize.toast('Already Clicked', 1000);
+        }
+    });
+
+}
+
+function checkSmall(currentSmall, currBigPos) {
+    var min = getMinMax(currentSmall);
+    if(matrix[min] == matrix[min + 1] && matrix[min + 1] == matrix[min + 2] && !(matrix[min] == 2)) {
+        dispWin(min, min + 1, min + 2, currBigPos);
+    } else if(matrix[min + 3] == matrix[min + 4] && matrix[min + 4] == matrix[min + 5] && !(matrix[min + 3] == 2)) {
+        dispWin(min + 3, min + 4, min + 5, currBigPos);
+    } else if(matrix[min + 6] == matrix[min + 7] && matrix[min + 7] == matrix[min + 8] && !(matrix[min + 6] == 2)) {
+        dispWin(min + 6, min + 7, min + 8, currBigPos);
+    } else if(matrix[min] == matrix[min + 3] && matrix[min + 3] == matrix[min + 6] && !(matrix[min] == 2)) {
+        dispWin(min, min + 3, min + 6, currBigPos);
+    } else if(matrix[min + 1] == matrix[min + 4] && matrix[min + 4] == matrix[min + 7] && !(matrix[min + 1] == 2)) {
+        dispWin(min + 1, min + 4, min + 7, currBigPos);
+    } else if(matrix[min + 2] == matrix[min + 5] && matrix[min + 5] == matrix[min + 8] && !(matrix[min + 2] == 2)) {
+        dispWin(min + 2, min + 5, min + 8, currBigPos);
+    } else if(matrix[min] == matrix[min + 4] && matrix[min + 4] == matrix[min + 8] && !(matrix[min] == 2)) {
+        dispWin(min, min + 4, min + 8, currBigPos);
+    } else if(matrix[min + 2] == matrix[min + 4] && matrix[min + 4] == matrix[min + 6] && !(matrix[min + 2] == 2)) {
+        dispWin(min + 2, min + 4, min + 6, currBigPos);
+    } else if(minicount[currBigPos] == 9) {
+        $("#b" + currBigPos).empty().removeClass('visi').append('<img src="asset/draw.jpg" alt="" class="above xo-img-mega">');
+        big[currBigPos] = 3;
+        ++megacount;
+    }
+}
+
+function dispWin(po1, po2, po3, cBPos) {
+    if(player == 0) {
+        img = "xwin";
+        imgw = "x-mega";
+    } else if(player == 1) {
+        img = "owin";
+        imgw = "o-mega";
+    }
+    $("#" + po1).empty().append('<img src="asset/' + img + '.jpg" class="xo-img"></img>');
+    $("#" + po2).empty().append('<img src="asset/' + img + '.jpg" class="xo-img"></img>');
+    $("#" + po3).empty().append('<img src="asset/' + img + '.jpg" class="xo-img"></img>');
+    $("#b" + cBPos).empty().removeClass('visi').append('<img src="asset/' + imgw + '.jpg" alt="" class="above xo-img-mega">');
+    big[cBPos] = player;
+    ++megacount;
+    checkBig();
+}
+
+function nextBigPos(val) {
+    return val - Math.floor(val / 9) * 9;
+}
+
+function block(position) {
+    if(position == 9) {
+        for(var i = 0; i < 9; i++) {
+            $('#b' + i).removeClass('visi');
+        }
+        for(var i = 0; i < 81; i++) {
+            $('#' + i).css("background-color", "#FFF");
+        }
+    } else {
+        minipos = position * 9;
+        for(var i = 0; i < 81; i++) {
+            $('#' + i).css("background-color", "#FFF");
+        }
+        if(big[position] != 2) { // Open choice condition
+            //Block all - reset
+            for(var i = 0; i < 9; i++) {
+                if(big[i] == 2) {
+                    $('#b' + i).addClass('visi');
+                }
+            }
+            //Color setting for open choice
+            var bigCount = 0;
+            for(var i = 0; i < 81; i++) {
+                if(big[bigCount] != 2) {
+                    i += 8;
+                    bigCount++;
+                    continue;
+                }
+                if(player == 0) {
+                    $('#' + i).css("background-color", "#BBDEFB");
+                } else if(player == 1) {
+                    $('#' + i).css("background-color", "#FFCCBC");
+                }
+
+                if((i + 1) % 9 == 0) {
+                    bigCount++;
+                }
+            }
+        } else { //Closed specific big box
+            for(var i = 0; i < 9; i++) {
+                if(big[i] == 2) {
+                    $('#b' + i).removeClass('visi');
+                }
+            }
+            for(var i = minipos; i < minipos + 9; i++) {
+                if(player == 0) {
+                    $('#' + i).css("background-color", "#BBDEFB");
+                } else if(player == 1) {
+                    $('#' + i).css("background-color", "#FFCCBC");
+                }
+
+            }
+            $('#b' + position).addClass('visi');
+        }
+    }
+}
+
+function checkBig() {
+    if(big[0] == big[1] && big[1] == big[2] && !(big[0] == 2) && !(big[0] == 3)) {
+        dispBig(0, 1, 2);
+    } else if(big[3] == big[4] && big[4] == big[5] && !(big[3] == 2) && !(big[3] == 3)) {
+        dispBig(3, 4, 5);
+    } else if(big[6] == big[7] && big[7] == big[8] && !(big[6] == 2) && !(big[6] == 3)) {
+        dispBig(6, 7, 8);
+    } else if(big[0] == big[3] && big[3] == big[6] && !(big[0] == 2) && !(big[0] == 3)) {
+        dispBig(0, 3, 6);
+    } else if(big[1] == big[4] && big[4] == big[7] && !(big[1] == 2) && !(big[1] == 3)) {
+        dispBig(1, 4, 7);
+    } else if(big[2] == big[5] && big[5] == big[8] && !(big[2] == 2) && !(big[2] == 3)) {
+        dispBig(2, 5, 8);
+    } else if(big[0] == big[4] && big[4] == big[8] && !(big[0] == 2) && !(big[0] == 3)) {
+        dispBig(0, 4, 8);
+    } else if(big[2] == big[4] && big[4] == big[6] && !(big[2] == 2) && !(big[2] == 3)) {
+        dispBig(2, 4, 6);
+    } else if(megacount == 9) {
+        block(9);
+        $('#won-player').text("Game Draw!");
+        $('#modal2').modal('open');
+    }
+}
+
+function dispBig(po1, po2, po3) {
+    if(player == 1) {
+        imgw = "xwin-mega";
+        wonPlayer = "X won!";
+    } else if(player == 0) {
+        imgw = "owin-mega";
+        wonPlayer = "O won!";
+    }
+    $("#b" + po1).empty().append('<img src="asset/' + imgw + '.jpg" alt="" class="above xo-img-mega">');
+    $("#b" + po2).empty().append('<img src="asset/' + imgw + '.jpg" alt="" class="above xo-img-mega">');
+    $("#b" + po3).empty().append('<img src="asset/' + imgw + '.jpg" alt="" class="above xo-img-mega">');
+    block(9);
+    $('#won-player').text(wonPlayer);
+    $('#modal2').modal('open');
+}
+
+function getMinMax(val) {
+    return Math.floor(val / 9) * 9;
+
+}
+
+function getBig(val) {
+    return Math.floor(val / 9);
+}
+
+function updateContainer() {
+
+    var $containerWidth = $(window).width();
+    if($containerWidth < 1050) {
+
+        if((screen.height > screen.width) || $containerWidth < 570) {
+            $(".card-cust").addClass('visi');
+            $(".playstore").removeClass("right").addClass("center-page").removeClass('visi');
+        } else if($containerWidth >= 570) {
+            $(".card-cust").removeClass('visi');
+            $(".playstore").addClass("right").removeClass("center-page").addClass('visi');
+        }
+        $('.playstore').addClass('visi');
+    } else {
+
+        // if ($containerWidth >= 570) {
+        //     $(".card-cust").removeClass('visi');
+        $(".playstore").addClass("right").removeClass("center-page").addClass('visi');
+        // }
+        // $('.playstore').removeClass('visi');
+    }
+
+
+
+}
+
+$('#logout').click(function(event) {
+    firebase.auth().signOut();
+    window.location.replace("/logout");
+});
---
+++ server.js
@@ -0,0 +1,58 @@
+var express = require('express');
+var app = express();
+var cp = require('cookie-parser');
+var firebase = require("firebase");
+var bodyParser = require('body-parser')
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+var config = {
+    apiKey: "AIzaSyD4hFHqjK0g8dDh44tkWpRXp5Jo__SW16A",
+    authDomain: "beat-bd6ed.firebaseapp.com",
+    databaseURL: "https://beat-bd6ed.firebaseio.com",
+    projectId: "beat-bd6ed",
+    storageBucket: "beat-bd6ed.appspot.com",
+    messagingSenderId: "190578562172"
+};
+firebase.initializeApp(config);
+
+var port = process.env.PORT || 8080;
+
+app.use(cp());
+var patho = __dirname + '/Uncompressed/';
+app.get('/', function(req, res) {
+    // res.clearCookie('token');
+    if(req.cookies.token) {
+        res.sendFile(patho + 'index.html');
+    } else {
+        res.redirect('/login');
+    }
+});
+app.use(express.static(patho));
+
+app.get('/login', function(req, res) {
+    if(!req.cookies.token) {
+        res.sendFile(patho + 'login.html');
+    } else {
+        res.redirect('/');
+    }
+});
+
+app.get('/logout',function (req,res) {
+    res.clearCookie('token');
+    res.redirect('/login');
+})
+
+app.post('/login', function(req, res) {
+    var token = req.body.token;
+    res.cookie('token', token);
+    res.setHeader('Content-Type', 'application/json');
+    res.send(JSON.stringify({ a: 1 }));
+    console.log('Redirected to /');
+});
+
+app.listen(port, function() {
+    console.log('app running');
+});
